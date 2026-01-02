8a7681357616d67cd27ac5e6caccf4e5dcaecf3b
khangtruong-ui <khang.truong@gmail.com>
2026-01-02 17:36:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,4 +12,4 @@ echo "====== PIP INSTALL EVERYTHING ====="
 
 pip install -q jax[tpu] flax optax transformers datasets diffusers==0.36 torch torchvision Pillow matplotlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
-cp attention_flax.py /home/$(whoami)/.local/lib/python3.10/site-packages/diffusers/models/attention_flax.py
+cp attention_flax.py $(pip list -v | grep diffusers | awk '{print $3}')diffusers/models/attention_flax.py
