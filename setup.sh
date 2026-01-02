dec11e834460fb9d923096ae4025cda8ca22ec69
khangtruong-ui <khang.truong@gmail.com>
2026-01-02 14:19:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ gsutil cp -r ./sd-full-finetuned/test.txt gs://khang-sd-ft/full
 
 gsutil -m cp -r gs://khang-sd-ft/full/* ./sd-full-finetuned
 
-pip install -q jax[tpu] flax optax transformers datasets diffusers==0.36 torch torchvision PIL matplotlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install -q jax[tpu] flax optax transformers datasets diffusers==0.36 torch torchvision Pillow matplotlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 cp attention_flax.py /home/khang/.local/lib/python3.10/site-packages/diffusers/models/attention_flax.py
 cp pipeline_flax_stable_diffusion.py ~/.local/lib/python3.11/site-packages/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
