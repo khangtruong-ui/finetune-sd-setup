52374ed78b3e1bdea6a2843aad13050ff4273008
khangtruong-ui <khang.truong@gmail.com>
2026-01-02 19:36:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "===== TEST GCS CONNECTION ====="
 mkdir -p ./sd-full-finetuned
-chmod -R 777 ./sd-full-finetuned
+chmod -R 777 .
 echo "Test GCS connection" > ./sd-full-finetuned/test.txt
 gsutil cp -r ./sd-full-finetuned/test.txt gs://khang-sd-ft/full
 
