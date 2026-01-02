d90f415a9293b082a0223d65756d7f069953f555
khangtruong-ui <khang.truong@gmail.com>
2026-01-02 17:15:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
-
+echo "===== TEST GCS CONNECTION ====="
 mkdir -p ./sd-full-finetuned
 echo "Test GCS connection" > ./sd-full-finetuned/test.txt
 gsutil cp -r ./sd-full-finetuned/test.txt gs://khang-sd-ft/full
 
 gsutil -m cp -r gs://khang-sd-ft/full/* ./sd-full-finetuned
+
+echo "====== PIP INSTALL EVERYTHING ====="
 
 pip install -q jax[tpu] flax optax transformers datasets diffusers==0.36 torch torchvision Pillow matplotlib -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
