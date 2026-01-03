e22dc839980dd6bbd5cdfd21bea264fcf442e8ae
khangtruong-ui <khang.truong@gmail.com>
2026-01-03 22:47:47 +0700

    Update full_retrain_distributed.py

--- full_retrain_distributed.py
+++ full_retrain_distributed.py
@@ -453,7 +453,7 @@ def main():
 
     def preprocess_train(examples):
         images = [image.convert("RGB") for image in examples[image_column]]
-        examples["pixel_values"] = [train_transforms(image) for image in images]
+        examples["pixel_values"] = jnp.array([train_transforms(image) for image in images])
         examples["input_ids"] = tokenize_captions(examples)
 
         return examples
