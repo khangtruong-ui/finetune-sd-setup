886368afe6b5d5380afdedc87d3fa196dac0a4e1
khangtruong-ui <khang.truong@gmail.com>
2026-01-04 08:27:52 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 echo "===== PWD: $(pwd) ====="
 echo "Script argument: $1"
+chmod +x setup.sh
+chmod +x full_retrain.sh
 
-sh ./setup.sh "$1" > setup.log 2>&1
-sh ./full_retrain.sh > retrain.log 2>&1
+./setup.sh "$1" > setup.log 2>&1
+./full_retrain.sh > retrain.log 2>&1
 gsutil cp *.log gs://khang-sd-ft/log
