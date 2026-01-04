fc01c838dc88902f6d7e0e41e0d6e1f613f1bf1f
khangtruong-ui <khang.truong@gmail.com>
2026-01-04 13:49:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "===== TEST GCS CONNECTION ====="
 mkdir -p ./sd-full-finetuned
-chmod -R 777 .
+
 echo "Test GCS connection" > ./sd-full-finetuned/test.txt
 
 gsutil cp -r ./sd-full-finetuned/test.txt gs://khang-sd-ft/full
@@ -14,5 +14,6 @@ echo "====== PIP INSTALL EVERYTHING ====="
 
 pip install -q jax[tpu] flax optax transformers==4.57.3 datasets diffusers==0.36 torch torchvision Pillow matplotlib grain -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 echo "===== DONE PIP INSTALL EVERYTHING ====="
+chmod -R 777 .
 # cp attention_flax.py $(pip list -v | grep diffusers | awk '{print $3}')/diffusers/models/attention_flax.py
 
