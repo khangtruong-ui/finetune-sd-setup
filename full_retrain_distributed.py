4727a72bc9f13b6550736e26f2dedb8e593333ef
khangtruong-ui <khang.truong@gmail.com>
2026-01-04 11:35:39 +0700

    Update full_retrain_distributed.py

--- full_retrain_distributed.py
+++ full_retrain_distributed.py
@@ -54,7 +54,7 @@ logger = logging.getLogger(__name__)
 
 jax.distributed.initialize()
 
-mesh = Mesh(np.array(jax.devices()).reshape((-1, 2)), ('data', 'kernel',))
+mesh = Mesh(np.array(jax.local_devices()).reshape((-1, 1)), ('data', 'kernel',))
 kernel_sharding = NamedSharding(mesh, P(None, 'kernel'))
 conv_sharding = NamedSharding(mesh, P(None, None, None, 'kernel'))
 sharding = NamedSharding(mesh, P('data'))
