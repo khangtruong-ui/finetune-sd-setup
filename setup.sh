96558a55d749c07932334f58428c702d81174053
khangtruong-ui <khang.truong@gmail.com>
2026-01-04 21:35:41 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ mkdir -p ./sd-full-finetuned
 
 echo "Test GCS connection" > ./sd-full-finetuned/test.txt
 
-gsutil cp -r ./sd-full-finetuned/test.txt gs://khang-sd-ft/full
+gsutil cp -r ./sd-full-finetuned/test.txt $SAVE_DIR/full
 
-gsutil -m cp -r gs://khang-sd-ft/original_weights/* ./sd-full-finetuned
+gsutil -m cp -r $SAVE_DIR/original_weights/* ./sd-full-finetuned
 
 echo "====== PIP INSTALL EVERYTHING ====="
 
