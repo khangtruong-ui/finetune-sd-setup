72a6ac9c770f85846268e4bd4ccfbc92d5afb680
khangtruong-ui <khang.truong@gmail.com>
2026-01-04 17:24:45 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
+
 echo "===== PWD: $(pwd) ====="
 chmod +x setup.sh
 chmod +x full_retrain.sh
 
+echo "===== SETUP START ====="
 ./setup.sh > setup.log 2>&1
+echo "===== RUN TASK ====="
 ./full_retrain.sh > retrain.log 2>&1
 gsutil cp *.log gs://khang-sd-ft/log
