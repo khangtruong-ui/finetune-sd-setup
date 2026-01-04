b1dab8402384498b82cc6dcbe180bdc92265e544
khangtruong-ui <khang.truong@gmail.com>
2026-01-04 14:20:40 +0700

    Update full_retrain.sh

--- full_retrain.sh
+++ full_retrain.sh
@@ -5,7 +5,7 @@ runuser -u 'khang_truong' -- python full_retrain_distributed.py \
     --dataset_name="KhangTruong/NWPU_Split" \
     --resolution=256 --center_crop --random_flip \
     --train_batch_size=8 \
-    --num_train_epochs=300 \
+    --num_train_epochs=400 \
     --learning_rate=1e-05 \
     --max_grad_norm=1 \
     --revision="flax"\
