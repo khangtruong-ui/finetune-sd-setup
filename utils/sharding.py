79ab9d8bf38e5950f693f07112a8bf9b3c071cb4
khangtruong-ui <khang.truong@gmail.com>
2026-01-06 14:46:49 +0700

    Update sharding.py

--- utils/sharding.py
+++ utils/sharding.py
@@ -4,16 +4,16 @@ import jax.numpy as jnp
 from jax.sharding import Mesh, NamedSharding, PartitionSpec as P
 import numpy as np
 
-def setup_sharding():
-    devices = np.array(jax.devices()).reshape(-1, 1)
-    mesh = Mesh(devices, ('data', 'model'))
-    return mesh, {
-        "data": NamedSharding(mesh, P('data')),
-        "model": NamedSharding(mesh, P(None, 'model')),
-        "replicated": NamedSharding(mesh, P()),
-    }
+mesh = Mesh(np.array(jax.devices()).reshape((-1, 1)), ('data', 'kernel',))
+kernel_sharding = NamedSharding(mesh, P(None, 'kernel'))
+conv_sharding = NamedSharding(mesh, P(None, None, None, 'kernel'))
+sharding = NamedSharding(mesh, P('data'))
+non_sharding = no_sharding = NamedSharding(mesh, P())
 
-def shard_params(params, sharding_rules):
-    def _shard(x):
-        return jax.device_put(x, sharding_rules["replicated"])  # Customize per-param if needed
-    return jax.tree_map(_shard, params)
+
+def distribute_device(tensor, sharding, replicate=False):
+    if type(tensor) in [int, float, str]:
+        return tensor
+    global_shape = (tensor.shape[0] * jax.process_count(),) + tensor.shape[1:] if not replicate else tensor.shape
+    global_array = jax.make_array_from_process_local_data(sharding, tensor, global_shape)
+    return global_array
