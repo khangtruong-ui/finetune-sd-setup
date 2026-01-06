9e6f075be621e189e586c4f96dfe7a46c03ac58e
khangtruong-ui <khang.truong@gmail.com>
2026-01-06 21:17:10 +0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ python train.py \
     --pretrained_model_name_or_path="./sd-full-finetuned" \
     --dataset_name="KhangTruong/NWPU_Split" \
     --resolution=256 --center_crop --random_flip \
-    --train_batch_size=32 \
+    --train_batch_size=64 \
     --num_train_epochs=1000 \
     --learning_rate=1e-05 \
     --max_grad_norm=1 \
