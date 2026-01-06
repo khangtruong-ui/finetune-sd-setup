5a12bba7cd7bd518b7ba80958a1c4865918024e0
khangtruong-ui <khang.truong@gmail.com>
2026-01-06 10:09:36 +0700

    Update train.py

--- train.py
+++ train.py
@@ -1,11 +1,9 @@
 # train.py
 from config.training_config import get_config
 from training.trainer import Trainer
-from utils.logging import setup_logging
 
 def main():
     config = get_config()
-    setup_logging()
 
     trainer = Trainer(config)
     trainer.train()
