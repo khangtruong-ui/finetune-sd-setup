1b72733e7460e01e2a9c4c1dc71b2e448e7a92be
khangtruong-ui <khang.truong@gmail.com>
2026-01-06 22:24:06 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -e
 echo "===== SETUP START ====="
 sh ./setup.sh > setup.log 2>&1
 echo "===== RUN TASK ====="
-sh ./train.sh > retrain.log 2>&1
+sh ./train.sh 2>&1 | grep -v "deprecated" > retrain.log 
 gsutil cp *.log $SAVE_DIR/log
