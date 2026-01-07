971da09e735166ff894ba8bb14672db79591cb26
khangtruong-ui <khang.truong@gmail.com>
2026-01-07 13:55:19 +0700

    Update trainer.py

--- training/trainer.py
+++ training/trainer.py
@@ -138,7 +138,7 @@ class Trainer:
                     if global_step >= self.max_steps:
                         break
                 # Checkpoint every 50 epochs or at the end
-                if (epoch + 1) % 100 == 0 or (epoch + 1) == self.config.num_train_epochs:
+                if (epoch + 1) % 50 == 0 or (epoch + 1) == self.config.num_train_epochs:
                     save_checkpoint(
                         self.config,
                         epoch,
